e47063eb96d06f7f99d2d6f96e8540f1a93f2ebd
misuy <mperedriy@bk.ru>
2022-10-07 02:25:30 +0300

    fix2

--- attempts_table.php
+++ attempts_table.php
@@ -9,12 +9,15 @@
         <td>execution time (ns)</td>
     </tr>
     <?php
-        foreach ($attempts as $curAttempt) {
-            if ($attempt != null) { 
-                if ($curAttempt["id"] == $attempt["id"]) { echo "<tr class=\"current_attempt_table_row attempts_table_row\"><td><a href=\"/?attemptId={$curAttempt["id"]}\">{$curAttempt["id"]}</a></td><td>{$curAttempt["x"]}</td><td>{$curAttempt["y"]}</td><td>{$curAttempt["r"]}</td><td>{$curAttempt["result"]}</td><td>{$curAttempt["current_server_time"]}</td><td>{$curAttempt["execution_time"]}</td></tr>"; } 
+        if ($attempts != null)
+        {
+            foreach ($attempts as $curAttempt) {
+                if ($attempt != null) { 
+                    if ($curAttempt["id"] == $attempt["id"]) { echo "<tr class=\"current_attempt_table_row attempts_table_row\"><td><a href=\"/?attemptId={$curAttempt["id"]}\">{$curAttempt["id"]}</a></td><td>{$curAttempt["x"]}</td><td>{$curAttempt["y"]}</td><td>{$curAttempt["r"]}</td><td>{$curAttempt["result"]}</td><td>{$curAttempt["current_server_time"]}</td><td>{$curAttempt["execution_time"]}</td></tr>"; } 
+                    else { echo "<tr class=\"attempts_table_row\"><td><a href=\"/?attemptId={$curAttempt["id"]}\">{$curAttempt["id"]}</a></td><td>{$curAttempt["x"]}</td><td>{$curAttempt["y"]}</td><td>{$curAttempt["r"]}</td><td>{$curAttempt["result"]}</td><td>{$curAttempt["current_server_time"]}</td><td>{$curAttempt["execution_time"]}</td></tr>"; }
+                }
                 else { echo "<tr class=\"attempts_table_row\"><td><a href=\"/?attemptId={$curAttempt["id"]}\">{$curAttempt["id"]}</a></td><td>{$curAttempt["x"]}</td><td>{$curAttempt["y"]}</td><td>{$curAttempt["r"]}</td><td>{$curAttempt["result"]}</td><td>{$curAttempt["current_server_time"]}</td><td>{$curAttempt["execution_time"]}</td></tr>"; }
             }
-            else { echo "<tr class=\"attempts_table_row\"><td><a href=\"/?attemptId={$curAttempt["id"]}\">{$curAttempt["id"]}</a></td><td>{$curAttempt["x"]}</td><td>{$curAttempt["y"]}</td><td>{$curAttempt["r"]}</td><td>{$curAttempt["result"]}</td><td>{$curAttempt["current_server_time"]}</td><td>{$curAttempt["execution_time"]}</td></tr>"; }
         }
     ?>
 </table>
--- db_manager.php
+++ db_manager.php
@@ -1,53 +1,90 @@
 <?php
     class DbManager {
-        private $db;
-        private $maxId = 0;
+        private $fileName = "./attempts";
+        private $maxId = -1;
 
-        public function __construct() {
-            $this->db = new SQLite3("attempts.db");
-            $this->db->exec("CREATE TABLE IF NOT EXISTS attempts (id INTEGER PRIMARY KEY, x REAL, y REAL, r REAL, result TEXT, current_server_time TEXT, execution_time TEXT)");
-            $stmt = $this->db->prepare("SELECT * FROM attempts");
-            $result = $stmt->execute();
-            while (true) {
-                $array = $result->fetchArray();
-                if (!($array)) break;
-                if ($array["id"] > $this->maxId) { $this->maxId = $array["id"]; }
+        private function parseCsvString($string) {
+            if ($string != "")
+            {
+                $substrings = explode(";", $string);
+                $resultArray = array();
+                $resultArray["id"] = intval($substrings[0]);
+                $resultArray["x"] = floatval($substrings[1]);
+                $resultArray["y"] = floatval($substrings[2]);
+                $resultArray["r"] = floatval($substrings[3]);
+                $resultArray["result"] = $substrings[4];
+                $resultArray["current_server_time"] = $substrings[5];
+                $resultArray["execution_time"] = $substrings[6];
+                return($resultArray);
             }
-        }   
-
-        private function resultSetToArray($result) {
-            $array = [];
-            while ($row = $result->fetchArray()) {
-                $array[] = $row;
-            }
-            return($array);
+            else return(null);
         }
 
+        private function parseCsvFile($data) {
+            if ($data == null) return(null);
+            else {
+                $resultArray = array();
+                $strings = explode("\n", $data);
+                foreach ($strings as $string) {
+                    $parsedString = $this->parseCsvString($string);
+                    if ($parsedString != null) array_push($resultArray, $parsedString);
+                }
+                return($resultArray);
+            }
+        }
+
+
+        private function createCsvString(int $id, float $x, float $y, float $r, string $result, $startOfExecutionTime) {
+            $current_server_time = date("Y-m-d H:i:s", time());
+            $executionTime = strval(hrtime()[1] - $startOfExecutionTime);
+            $string = "{$id};{$x};{$y};{$r};{$result};{$current_server_time};{$executionTime}";
+            return($string);
+        }
+
+        public function __construct() {
+            $file = fopen($this->fileName, "r");
+            $data = null;
+            if (filesize($this->fileName) > 0) $data = fread($file, filesize($this->fileName));
+            $attempts = $this->parseCsvFile($data);
+            if ($attempts != null)
+            {
+                foreach ($attempts as $attempt) {
+                    if ($attempt["id"] > $this->maxId) { $this->maxId = $attempt["id"]; }
+                }
+            }
+            fclose($file);
+        }   
+
         public function getAllAttempts() {
-            $stmt = $this->db->prepare("SELECT * FROM attempts");
-            $result = $stmt->execute();
-            return($this->resultSetToArray($result));
+            $file = fopen($this->fileName, "r");
+            $data = null;
+            if (filesize($this->fileName) > 0) $data = fread($file, filesize($this->fileName));
+            $attempts = $this->parseCsvFile($data);
+            fclose($file);
+            return($attempts);
         }
 
         public function getAttemptById($id) {
-            $stmt = $this->db->prepare("SELECT * FROM attempts WHERE id=(:id)");
-            $stmt->bindParam(":id", $id);
-            $result = $stmt->execute();
-            return($this->resultSetToArray($result)[0]);
+            $file = fopen($this->fileName, "r");
+            $data = null;
+            if (filesize($this->fileName) > 0) $data = fread($file, filesize($this->fileName));
+            $attempts = $this->parseCsvFile($data);
+            $searchingAttempt = null;
+            if ($attempts != null) {
+                foreach ($attempts as $attempt) {
+                    if ($attempt["id"] == $id) $searchingAttempt = $attempt;
+                }
+            }
+            fclose($file);
+            return($searchingAttempt);
         }
 
         public function addAttempt(int $id, float $x, float $y, float $r, string $result, $startOfExecutionTime) {
-            $stmt = $this->db->prepare("INSERT INTO attempts (id, x, y, r, result, current_server_time, execution_time) VALUES (:id, :x, :y, :r, :result, :current_server_time, :execution_time)");
-            $stmt->bindParam(":id", $id);
-            $stmt->bindParam(":x", $x);
-            $stmt->bindParam(":y", $y);
-            $stmt->bindParam(":r", $r);
-            $stmt->bindParam(":result", $result);
-            $current_server_time = date("Y-m-d H:i:s", time());
-            $stmt->bindParam(":current_server_time", $current_server_time);
-            $executionTime = strval(hrtime()[1] - $startOfExecutionTime);
-            $stmt->bindParam(":execution_time", $executionTime);
-            $result = $stmt->execute();
+            $string = $this->createCsvString($id, $x, $y, $r, $result, $startOfExecutionTime);
+            $file = fopen($this->fileName, "a");
+            fwrite($file, $string);
+            fwrite($file, "\n");
+            fclose($file);
         }
 
         public function getIdForNewAttempt() {
