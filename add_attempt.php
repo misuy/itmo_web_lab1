674eb9a9467fda38c583b0b731a8ccdaf3870bde
misuy <mperedriy@bk.ru>
2022-10-07 03:25:38 +0300

    fix9

--- add_attempt.php
+++ add_attempt.php
@@ -3,23 +3,14 @@
     include("check_attempt.php");
     include("db_manager.php");
 
-    $x = null;
-    $y = null;
-    $r = null;
-    if (isset($_POST["x"])) { $x = floatval($_POST["x"]); }
-    if (isset($_POST["y"])) { $y = floatval($_POST["y"]); }
-    if (isset($_POST["r"])) { $r = floatval($_POST["r"]); }
-    echo $x;
-    echo $y;
-    echo $r;
-    if (($x != null) & ($y != null) & ($r != null))
-    {
-        $result = checkAttempt($x, $y, $r);
-        $dbManager = new DbManager();
-        $id = $dbManager->getIdForNewAttempt();
-        $dbManager->addAttempt($id, $x, $y, $r, $result, $startOfExecutionTime);
-        header("Location: https://se.ifmo.ru/~s335120/itmo_web_lab1/?attemptId={$id}");
-    }
-    else header("Location: https://se.ifmo.ru/~s335120/itmo_web_lab1/");
+    $x = floatval($_POST["x"]);
+    $y = floatval($_POST["y"]);
+    $r = floatval($_POST["r"]);
+    
+    $result = checkAttempt($x, $y, $r);
+    $dbManager = new DbManager();
+    $id = $dbManager->getIdForNewAttempt();
+    $dbManager->addAttempt($id, $x, $y, $r, $result, $startOfExecutionTime);
+    header("Location: https://se.ifmo.ru/~s335120/itmo_web_lab1/?attemptId={$id}");
     exit();
 ?>
